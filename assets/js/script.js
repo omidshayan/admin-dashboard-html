49ec1e927deb76e3ced883fe20e2ac331b9492f6
unknown <omidshayan.afg@gmail.com>
2024-04-02 19:36:47 +0430

    add appbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
 const sidebar = document.querySelector('.sidebar');
 const hamber = document.querySelector('.hamber');
 const mainContent = document.querySelector('.content');
+const appbar = document.querySelector('.appbar');
 const menuToggle = document.querySelector('#menu-toggle');
 
 hamber.addEventListener('click', function(){
      sidebar.classList.toggle('active')
      mainContent.classList.toggle('active');
+     appbar.classList.toggle('active');
      menuToggle.classList.toggle('active')
 })
 
 menuToggle.addEventListener('click', function(){
      sidebar.classList.toggle('active')
      mainContent.classList.toggle('active');
+     appbar.classList.toggle('active');
      menuToggle.classList.toggle('active')
 })
 
