524e7dcfb7ad7aa7009afc509b0e891224f34e7c
unknown <omidshayan.afg@gmail.com>
2024-08-12 23:57:58 +0430

    add menu

--- accourdion-select/accourdion.js
+++ accourdion-select/accourdion.js
@@ -1,45 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var mainCheckboxes = document.querySelectorAll('.main-checkbox-select');
-    var innerCheckboxes = document.querySelectorAll('.inner-checkbox-select');
+document.addEventListener("DOMContentLoaded", function () {
+  var mainCheckboxes = document.querySelectorAll(".main-checkbox-select");
+  var innerCheckboxes = document.querySelectorAll(".inner-checkbox-select");
 
-    innerCheckboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            var parentId = this.getAttribute('data-parent');
-            var parentCheckbox = document.getElementById(parentId);
+  innerCheckboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function () {
+      var parentId = this.getAttribute("data-parent");
+      var parentCheckbox = document.getElementById(parentId);
 
-            var allChecked = Array.from(document.querySelectorAll(`input[data-parent='${parentId}']`)).every(cb => cb.checked);
-            var anyChecked = Array.from(document.querySelectorAll(`input[data-parent='${parentId}']`)).some(cb => cb.checked);
+      var allChecked = Array.from(
+        document.querySelectorAll(`input[data-parent='${parentId}']`)
+      ).every((cb) => cb.checked);
+      var anyChecked = Array.from(
+        document.querySelectorAll(`input[data-parent='${parentId}']`)
+      ).some((cb) => cb.checked);
 
-            if (anyChecked) {
-                parentCheckbox.checked = true;
-            } else {
-                parentCheckbox.checked = false;
-            }
-        });
+      if (anyChecked) {
+        parentCheckbox.checked = true;
+      } else {
+        parentCheckbox.checked = false;
+      }
     });
+  });
 
-    mainCheckboxes.forEach(function(checkbox) {
-        checkbox.style.display = 'none';
+  mainCheckboxes.forEach(function (checkbox) {
+    checkbox.style.display = "none";
+  });
+
+  var accordionButtons = document.querySelectorAll(".accordion-button-select");
+  accordionButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      var content = this.nextElementSibling;
+      var isOpen = content.style.maxHeight;
+
+      // بستن تمامی منوهای باز قبل از باز کردن منوی جدید
+      var allContents = document.querySelectorAll(".accordion-content-select");
+      allContents.forEach(function (cont) {
+        cont.style.maxHeight = null;
+      });
+
+      if (isOpen) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
     });
-
-    var accordionButtons = document.querySelectorAll('.accordion-button-select');
-    accordionButtons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            var content = this.nextElementSibling;
-            var isOpen = content.style.maxHeight;
-
-            // بستن تمامی منوهای باز قبل از باز کردن منوی جدید
-            var allContents = document.querySelectorAll('.accordion-content-select');
-            allContents.forEach(function(cont) {
-                cont.style.maxHeight = null;
-            });
-
-            if (isOpen) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    });
+  });
 });
