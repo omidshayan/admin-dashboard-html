13235c73dcf40e9383297801022e0631ab111825
unknown <omidshayan.afg@gmail.com>
2024-08-27 23:30:20 +0430

    modal

--- modal/js.js
+++ modal/js.js
@@ -1,4 +1,16 @@
 function toggleDarkMode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
+function selectPayment(paymentType) {
+  var receiptButton = document.getElementById("receiptButton");
+  var withdrawalButton = document.getElementById("withdrawalButton");
+
+  if (paymentType === "receipt") {
+    receiptButton.classList.add("selected", "receipt");
+    withdrawalButton.classList.remove("selected", "withdrawal");
+  } else if (paymentType === "withdrawal") {
+    receiptButton.classList.remove("selected", "receipt");
+    withdrawalButton.classList.add("selected", "withdrawal");
   }
+}
